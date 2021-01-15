77c1fce5141ed824981d65ce8cc0ea7bd7df7cc7
jiang-1990“git config --global user.email 1914251357@qq.comgit config --listgit config --global user.name jiang-1990“ <1914251357@qq.com>
2021-01-15 15:25:19 +0800

    进入home组件的token验证

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -3,23 +3,111 @@
     <el-header class="header">
       <el-row>
         <el-col :span="4"><h3>后台管理系统</h3></el-col>
-        <el-col :span="18"><div class="grid-content bg-purple-light middle">欢迎使用</div></el-col>
+        <el-col :span="18"
+          ><div class="grid-content bg-purple-light middle">
+            欢迎使用
+          </div></el-col
+        >
         <el-col :span="2">
-            <div class="grid-content bg-purple">
-                <a class="loginout" href="#">退出</a>
-            </div>
+          <div class="grid-content bg-purple">
+            <a class="loginout" href="#">退出</a>
+          </div>
         </el-col>
       </el-row>
     </el-header>
     <el-container>
-      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-aside class="aside" width="200px">
+        <el-menu
+            :unique-opened='true'
+        >
+            <!--1-->
+            <el-submenu index="1">
+                <template slot="title">
+                    <i class="el-icon-user"></i>
+                    <span>用户管理</span>
+                </template>    
+                <el-menu-item index="1-1">
+                    <i class="el-icon-document-copy"></i>
+                    <span>用户列表</span>
+                </el-menu-item>
+            </el-submenu>
+            <!--2-->
+            <el-submenu index="2">
+                <template slot="title">
+                    <i class="el-icon-setting"></i>
+                    <span>权限管理</span>
+                </template>    
+                <el-menu-item index="2-1">
+                    <i class="el-icon-document-copy"></i>
+                    <span>角色列表</span>
+                </el-menu-item>
+                <el-menu-item index="2-2">
+                    <i class="el-icon-document-copy"></i>
+                    <span>权限列表</span>
+                </el-menu-item>
+            </el-submenu>
+            <!--3-->
+            <el-submenu index="3">
+                <template slot="title">
+                    <i class="el-icon-goods"></i>
+                    <span>商品管理</span>
+                </template>    
+                <el-menu-item index="3-1">
+                    <i class="el-icon-document-copy"></i>
+                    <span>商品列表</span>
+                </el-menu-item>
+                <el-menu-item index="3-2">
+                    <i class="el-icon-data-analysis"></i>
+                    <span>分类参数</span>
+                </el-menu-item>
+                <el-menu-item index="3-3">
+                    <i class="el-icon-c-scale-to-original"></i>
+                    <span>商品分类</span>
+                </el-menu-item>
+            </el-submenu>
+            <!--4-->
+            <el-submenu index="4">
+                <template slot="title">
+                    <i class="el-icon-folder-checked"></i>
+                    <span>订单管理</span>
+                </template>    
+                <el-menu-item index="4-1">
+                    <i class="el-icon-document-copy"></i>
+                    <span>订单列表</span>
+                </el-menu-item>
+            </el-submenu>
+            <!--5-->
+            <el-submenu index="5">
+                <template slot="title">
+                    <i class="el-icon-pie-chart"></i>
+                    <span>数据统计</span>
+                </template>    
+                <el-menu-item index="5-1">
+                    <i class="el-icon-coin"></i>
+                    <span>数据报表</span>
+                </el-menu-item>
+            </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-main class="main">Main</el-main>
     </el-container>
   </el-container>
 </template>
 
 <script>
-export default {};
+export default {
+    //newVue 之前自动触发
+    beforeCreate(){
+        //获取token
+        const token = localStorage.getItem('token')
+         //if token 有 > 继续渲染组件
+         console.log(token)
+         //token 没有 > 登录
+        if(!token){
+            this.$router.puch({name:'login'})
+        }
+    }
+}
 </script>
 
 <style scoped>
@@ -30,19 +118,19 @@ export default {};
   background-color: #b3c0d1;
 }
 .aside {
-  background-color: #d3dce6;
+  
 }
 .main {
   background-color: #e9eef3;
 }
 /**头部样式 */
-.middle{
-    text-align: center;
-    line-height: 60px;
+.middle {
+  text-align: center;
+  line-height: 60px;
 }
-.loginout{
-    text-decoration: none;
-    line-height: 60px;
-    margin-left: 80px;
+.loginout {
+  text-decoration: none;
+  line-height: 60px;
+  margin-left: 80px;
 }
 </style>
